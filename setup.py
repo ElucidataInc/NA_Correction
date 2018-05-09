c86b93cbc851b68cd433faaff9a71e87928f3984
Sandesh <sandesh.timilsina@elucidata.io>
2018-05-09 18:48:41 +0530

    Corna version updated to 0.1.65

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
 	name='corna',
-    version='0.0.86',
+    version='0.1.65',
     description='Natural Abundance Correction Toolbox',
     long_description=readme(),
     packages = find_packages(),
