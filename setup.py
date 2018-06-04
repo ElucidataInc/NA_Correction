541de05d9be5bed3e3597d43546294ea70b3033b
Shubham Agrawal <shubham.agrawal@elucidata.io>
2018-06-04 08:14:23 +0000

    Revert "Corna Fix elmaven missing value to Develop (pull request #63)"
    This reverts pull request #63.
    
    > * Gsheet exception catch added
    > 
    > * Corna version updated to 0.0.88

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
 	name='corna',
-    version='0.1.65',
+    version='0.1.64',
     description='Natural Abundance Correction Toolbox',
     long_description=readme(),
     packages = find_packages(),
