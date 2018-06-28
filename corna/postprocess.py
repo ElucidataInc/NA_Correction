8f94bd0d1568aca2f652b9402af39a94c697f60c
14priyanka <priyanka.agarwal@elucidata.io>
2018-06-28 19:16:15 +0530

    fractional_enrichment function simplified.

--- corna/postprocess.py
+++ corna/postprocess.py
@@ -7,18 +7,21 @@ def replace_negatives(df):
     df['NA Corrected with zero']= df['NA Corrected'].clip(lower=0)
     return df
 
-def pool_total(df):
+def calculate_pool_total(df):
     """
-    This function calculates the pool total for each metabolite in a sample
-    Args:
-        df: data frame with corrected intensities
-    Returns: df: datframe which column consisting of pool total for each metabolite
-             total: pool total for a single metabolite.
+    This function calculates pool total
+    for all the metabolites in the input data file
 
+    Args:
+        df: dataframe for which pool total has to be calculated.
+    Returns:
+        final_df: dataframe which consists of the calculated values.
     """
-    total= df['NA Corrected with zero'].sum()
-    df['Pool_Total']= total
-    return df, total
+    df['Pool_total']=df['NA Corrected with zero']
+    df1= df.groupby(['Sample','Name'])['Pool_total'].sum().reset_index()
+    df.drop('Pool_total', axis=1, inplace=True)
+    df= df.merge(df1, on=['Sample', 'Name'])
+    return df
 
 def fractional_enrichment(df):
     """
@@ -30,22 +33,15 @@ def fractional_enrichment(df):
     Returns:
         final_df: dataframe which consists of the calculated values.
     """
+    final_df= pd.DataFrame()
     df= df.filter(['Sample', 'Name', 'Label','Formula', 'NA Corrected'])
     df= replace_negatives(df)
-    final_df= pd.DataFrame()
 
-    for samp in df.Sample.unique():
-        samp_df= df[df['Sample']== samp]
-        for metab in samp_df.Name.unique():
-            metab_df= samp_df[samp_df['Name']== metab]
-            metab_df, pooltotal= pool_total(metab_df)
-            metab_df['Fractional_enrichment']= metab_df['NA Corrected with zero']/pooltotal
-            final_df= final_df.append(metab_df)
-    final_df= final_df.fillna(0)
+    df= calculate_pool_total(df)
+    df['Fractional_enrichment']= df['NA Corrected with zero']/df['Pool_total']
+    final_df= df.fillna(0)
     if 'NA Corrected' in final_df.columns:
         final_df.drop(['NA Corrected'], axis=1, inplace=True)
     if 'NA Corrected with zero' in final_df.columns:
         final_df.drop(['NA Corrected with zero'], axis=1, inplace=True)
     return final_df
-
-
