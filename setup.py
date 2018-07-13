ca9428b5bc0ba26798d907d2015b48091d8e51a1
14priyanka <priyanka.agarwal@elucidata.io>
2018-07-13 11:40:18 +0530

    Package version updated for testing.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
 	name='corna',
-    version='0.0.322',
+    version='0.0.323',
     description='Natural Abundance Correction Toolbox',
     long_description=readme(),
     packages = find_packages(),
