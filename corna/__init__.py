11a909d752aa504a3c0c9ad6f11e3e93641b5a3c
14priyanka <priyanka.agarwal@elucidata.io>
2018-07-19 12:05:05 +0530

    Test cases updated.

--- corna/__init__.py
+++ corna/__init__.py
@@ -2,3 +2,5 @@
 from .helpers import read_file, json_to_df, filter_df, merge_multiple_dfs, get_na_value_dict, parse_polyatom, \
     get_global_isotope_dict
 #from .output import convert_to_df, save_to_csv, convert_to_df_nacorr, convert_to_df_nacorr_MSMS
+from .algorithms.nacorr_lcms import na_correction
+from .postprocess import replace_negatives, fractional_enrichment
--- tests/test_mq_parser.py
+++ tests/test_mq_parser.py
@@ -4,9 +4,7 @@ import pandas as pd
 from pandas.util.testing import assert_frame_equal
 import pytest
 
-# from corna.inputs.multiquant_parser import concat_txts_into_df
 from corna.inputs import multiquant_parser
-# from olmonk import basic_validation, data_validation
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 MQ_FILE_PATH = os.path.join(DIR_PATH, 'test_input_validation_data', "raw_mq.txt")
@@ -25,71 +23,32 @@ INPUT_FILES_WITHOUT_METADATA = {"mq_file_path": MQ_FILE_PATH,
                                 "mq_metadata_path": MQ_METADATA_PATH,
                                 "mq_sample_metadata_path": ''}
 
-# TODO : fixtures can be defined for many constants
+raw_df = pd.DataFrame({'Label': { 0: 'O18_191.0_111.0', 1: 'O18_193.0_113.0', 2: 'O18_195.0_115.0'},
+                        'Parent Formula':{ 0: 'C6H7O7', 1: 'C6H7O7', 2: 'C6H7O7'},
+                        'Formula': {0: 'C5H3O3',1: 'C5H3O3', 2: 'C5H3O3'}} )
 
+mass_df = pd.DataFrame({
+    'iso_mass': {0: 193.0, 1: 191.0},
+    'mol_mass': {0: 191, 1:191}
+})
+ 
 
-def test_get_instance():
-    """checks if this function returns instance of correct class
+def test_add_mass_and_no_of_atoms_info_frm_label():
+    result_df, isotracer = multiquant_parser.add_mass_and_no_of_atoms_info_frm_label(raw_df)
+    print result_df
+    No_of_labeled_atoms_parent = [0, 1, 2]
+    No_of_labeled_atoms_daughter = [0, 1, 2]
+    assert result_df['Number of labeled atoms parent'].tolist() == No_of_labeled_atoms_parent
+    assert result_df['Number of labeled atoms daughter'].tolist() == No_of_labeled_atoms_daughter
 
-    get_instance function takes input_files, validates all files,
-    and returns the instances of class having these files as output.
-    So, it test if returns instance is correct or not.
-    """
-    # :TODO: add more extensive testing for this function
-    raw_mq, metadata_mq, sample_metadata_mq = multiquant_parser.\
-                                                get_basic_validation_instance(INPUT_FILES)
+def test_get_num_labeled_atoms():
+    result = multiquant_parser.get_num_labeled_atoms('O18', mass_df['iso_mass'], mass_df['mol_mass'])
+    label_no= [1, 0]
+    assert result.tolist() == label_no
 
-    assert isinstance(raw_mq, basic_validation.BasicValidator)
-    assert not isinstance(raw_mq, data_validation.DataValidator)
-
-
-def test_get_set_from_df_column():
-    df = pd.read_table(MQ_FILE_PATH)
-    assert type(multiquant_parser.get_set_from_df_column(df, 'Area')) == set
-    assert len(multiquant_parser.get_set_from_df_column(df, 'Area')) == 7236
-
-
-def test_get_validated_df_and_logs():
-    # TODO: update test case as per the new params
-
-    raw_mq, metadata_mq, sample_metadata_mq, summary = multiquant_parser.\
-                                                get_validated_df_and_logs(INPUT_FILES)
-
-    assert len(raw_mq.logs['warnings']['message']) == 3126
-    assert len(metadata_mq.logs['warnings']['message']) == 0
-    assert raw_mq.df.shape == (11252, 6)
-    assert metadata_mq.df.shape == (75, 5)
-    assert sample_metadata_mq.df.shape == (96, 9)
-    assert summary == {'SampleData': {'summary': [{'value': u'Original Filename, Metadata, Sampletype, Glucose Conc, Gln/Glc, Activator, Time, Background Sample, Sample Name',
-                                                   'label': 'Fields in metadata'},
-                                                  {'value': 12, 'label': 'Number of background samples'}],
-                                      'title': 'SampleData'},
-                       'InputData': {'summary': [{'value': 116, 'label': 'Number of samples'},
-                                                 {'value': 11252, 'label': 'Number of rows'},
-                                                 {'value': 97, 'label': 'Number of metabolites'},
-                                                 {'value': 26, 'label': 'Number of cohorts'}],
-                                     'title': 'InputData'},
-                       'MetaData': {'summary': [{'value': u'C13', 'label': 'isotopic tracer'},
-                                                {'value': 75, 'label': 'Number of fragments'},
-                                                {'value': 12, 'label': 'Number of unlabeled fragments'}],
-                                    'title': 'MetaData'}}
-
-
-def test_get_validated_df_logs_without_sample_metadata():
-    # TODO: update test case as per the new params
-
-    raw_mq, metadata_mq, sample_metadata_mq, summary = multiquant_parser. \
-        get_validated_df_and_logs(INPUT_FILES_WITHOUT_METADATA)
-
-    assert len(raw_mq.logs['warnings']['message']) == 3126
-    assert len(metadata_mq.logs['warnings']['message']) == 0
-    assert raw_mq.df.shape == (11252, 6)
-    assert metadata_mq.df.shape == (75, 5)
-    assert sample_metadata_mq == None
-
-
-def test_filtered_raw_mq_df():
-    raw_mq = basic_validation.BasicValidator(MQ_FILE_PATH)
-    sample_metadata = basic_validation.BasicValidator(MQ_SAMPLE_METADATA_PATH)
-    with pytest.raises(Exception) as e:
-        multiquant_parser.get_filtered_raw_mq_df(raw_mq, sample_metadata)
+def test_get_parent_daughter_iso_mass_col_and_isotracer_from_label():
+    result_df = multiquant_parser.get_parent_daughter_iso_mass_col_and_isotracer_from_label(raw_df)
+    isotracer = ['O18', 'O18', 'O18']
+    iso_mass_parent = [191.0, 193.0, 195.0]
+    assert result_df['Isotopic Tracer'].tolist() == isotracer
+    assert result_df['isotopic_mass_parent'].tolist() == iso_mass_parent
