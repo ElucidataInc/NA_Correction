b0269c1509f3b2687881cf836b13516063b43058
gurunath_katagi <gurunath.katagi@elucidata.io>
2019-06-13 05:58:12 +0530

    background correction function checked

--- corna/algorithms/background_correction.py
+++ corna/algorithms/background_correction.py
@@ -54,10 +54,11 @@ def background_subtraction(input_intensity, noise):
         noise of cohort group    
     """
     intensity = input_intensity - noise
-    if intensity > 0:
-        return intensity
-    else:
-        return 0
+    return intensity
+    #if intensity > 0:
+    #    return intensity
+    #else:
+    #    return 0
 
 
 def background_correction(msms_df, list_of_replicates, isotope_dict=const.ISOTOPE_NA_MASS):
@@ -78,27 +79,40 @@ def background_correction(msms_df, list_of_replicates, isotope_dict=const.ISOTOP
         output_df: Background corrected dataframe
     """
     final_df, isotracer = multiquant_parser.add_mass_and_no_of_atoms_info_frm_label(msms_df)
+    #print(final_df.columns)
+    #print(isotracer)
     output_df = pd.DataFrame()
 
     na = get_isotope_na(isotracer[0], isotope_dict)
+    #print(na)
 
     final_df[const.UNLABELED] = 'False'
     final_df.loc[final_df[const.PARENT_NUM_LABELED_ATOMS] == 0, const.UNLABELED] = 'True'
+    #rint(final_df.head())
+    final_df.to_csv('final_df.csv')
 
     for metab in final_df.Name.unique():
+        #print(metab)
         metab_df = final_df[final_df[const.NAME_COL] == metab]
         unlabel_isotope_df = metab_df[metab_df[const.UNLABELED] == 'True']
+        metab_df.to_csv('metab_df.csv')
+        unlabel_isotope_df.to_csv('unlabel_isotope_df.csv')
 
         for frag in metab_df[multiquant.MQ_FRAGMENT].unique():
+            #print(frag)
             frag_df = metab_df[metab_df[multiquant.MQ_FRAGMENT] == frag]
+            #print(frag_df.head(2))
+            frag_df.to_csv('frag_df.csv')
             for replicate_group in list_of_replicates:
                 background_list = []
                 for each_replicate in replicate_group:
                     try:
                         unlabel_intensity = unlabel_isotope_df.loc[unlabel_isotope_df[const.SAMPLE_COL] == each_replicate,
-                                                                         const.INTENSITY_COL].iloc[0]
+                                                                         const.INTENSITY_COL].iloc[0]                                           
                     except:
                         unlabel_intensity = 0
+                    #print('unlabel intensity') 
+                    #print(each_replicate, unlabel_intensity) 
                     noise = background_noise(unlabel_intensity, na, frag_df[const.PARENT_NUM_ATOMS].unique()[0],
                                                     frag_df[const.PARENT_NUM_LABELED_ATOMS].unique()[0],
                                                     frag_df[const.DAUGHTER_NUM_ATOMS].unique()[0],
